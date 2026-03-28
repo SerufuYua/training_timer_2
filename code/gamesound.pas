ed6b38ce35712655591891c3fb1d3f359df7f4ef
@serufu_yua <serufu_yua@nya.com>
2026-03-28 12:12:40 +0500

    update sounds code description

--- code/gamesound.pas
+++ code/gamesound.pas
@@ -1,25 +1,10 @@
-{
-  Copyright 2023-2023 Andrzej Kilijański, Michalis Kamburelis.
-
-  This file is part of "Castle Game Engine".
-
-  "Castle Game Engine" is free software; see the file COPYING.txt,
-  included in this distribution, for details about the copyright.
-
-  "Castle Game Engine" is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-  ----------------------------------------------------------------------------
-}
-
 { Manage the repository of sounds (useful from any view).
 
   Usage:
   - Configure sounds using CGE editor by editing sounds.castle-component ,
   - From code:
     - Call once InitializeSounds (e.g. from ApplicationInitialize)
-    - Use NamedSound to load TCastleSound instances. }
+    - Use Play to play sound. }
 unit GameSound;
 
 interface
