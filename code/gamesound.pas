7c6c125121af30870438df589409f20c24e025d5
@serufu_yua <serufu_yua@nya.com>
2026-04-24 11:57:30 +0500

    new sound Tsyuin

--- code/gamesound.pas
+++ code/gamesound.pas
@@ -10,7 +10,7 @@ unit GameSound;
 interface
 
 type
-  TSoundType = (None, Start, Ending, Final, Warn, Init, BeeEeeEeep);
+  TSoundType = (None, Start, Ending, Final, Warn, Init, BeeEeeEeep, Tsyuin);
   TSfxType = (Intro, ClickMode, ClickAction, ClickEdit, ClickOk, ClickCancel,
               ClickDeny, PointerHover, ListHover, ClickStart, ClickStop,
               ClickWeb, Check, TapKey);
