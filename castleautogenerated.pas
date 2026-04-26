894a6bef5bfa49592e5645dac4c737ee0b5b9a73
@serufu_yua <serufu_yua@nya.com>
2026-04-26 08:55:33 +0500

    fix movement cursor and list animations

--- castleautogenerated.pas
+++ castleautogenerated.pas
@@ -23,7 +23,7 @@ uses CastleApplicationProperties, CastleWindow, CastleLog;
 initialization
   ApplicationProperties.ApplicationName := 'TrainingTimer2';
   ApplicationProperties.Caption := 'Training Timer 2: Zero Time';
-  ApplicationProperties.Version := '2.2';
+  ApplicationProperties.Version := '2.2.1';
 
   if not IsLibrary then
     Application.ParseStandardParameters;
